81cbbb1370e993daaad651a78c2a5131324eb753
Toby Newman <tobias-riesa@gmx.de>
2016-02-09 18:05:56 +0100

    added second scene, so it's possible to work parallel

--- Assets/toDo.cs
+++ Assets/toDo.cs
@@ -1,10 +1,10 @@
 ﻿/* to do list & ideas
 
 you can choose every unit and change to its FPS view to control it
-
 routing: a*
-
 recycliong of wrecks
+
+put units, buildings and objects NOT in raycast ignore layer, so you can usw the raycast with left-/rightclick
 
 camera:
 - vertical rotation limit
