5b4249f4d908dc59c1249ae3ba132bbad4db1ac9
Toby Newman <tobias-riesa@gmx.de>
2016-02-10 01:07:35 +0100

    updated 2do list

--- Assets/toDo.cs
+++ Assets/toDo.cs
@@ -6,8 +6,14 @@ recycliong of wrecks
 
 put units, buildings and objects NOT in raycast ignore layer, so you can usw the raycast with left-/rightclick
 
+Buildings:
+- smooth rotate with q & e (pressed at same time sets rotation speed to zero)
 MouseReaciton.cs
 - set hover/select materials
 > how to select??
+> serveral object selection with double click or marking area
+
+units:
+- UnitExample.cs with a list for each unit type (same with buildings?)
 
 */
