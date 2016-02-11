422418bb40dccd9b0c7d7a819838fb6fb1735be1
marvnie1994 <marvnie1994@googlemail.com>
2016-02-11 22:51:08 +0100

    Added UnserInterface.cs to Camera

---
+++ Assets/scripts/UserInterface.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInterface : MonoBehaviour {
+
+	private Rect Pos;
+	
+	// Use this for initialization
+	void OnGUI () {
+	//	GUI.Box ( Rect (0,0,100,100), "Text hier" );
+		GUI.Box(new Rect( 20, 20, 200, 100), "Ein UnserInterface wird folgen");
+	}
+	
+	void Update () {
+		OnGUI();
+	}
+}
