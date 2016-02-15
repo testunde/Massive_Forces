5496f652cc8eaf00093f9d8dd0b521298a162c27
Toby Newman <tobias-riesa@gmx.de>
2016-02-15 20:52:41 +0100

    updated UML, 2do list, add testUnit from blender

--- Assets/toDo.cs
+++ Assets/toDo.cs
@@ -4,6 +4,8 @@ you can choose every unit and change to its FPS view to control it
 routing: a*
 recycliong of wrecks
 quit button: destroy user interface :D
+game statistics at the end of a game
+
 put units, buildings and objects NOT in raycast ignore layer, so you can usw the raycast with left-/rightclick
 OR: use the OnMouseEnter() methods
 
