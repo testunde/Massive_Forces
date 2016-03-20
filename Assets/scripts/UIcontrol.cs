2b77a97c2ad11054c691e564d331ac95044544ef
marvnie1994 <marvnie1994@googlemail.com>
2016-03-20 18:16:20 +0100

    changed onClick in UIControl...

--- Assets/scripts/UIcontrol.cs
+++ Assets/scripts/UIcontrol.cs
@@ -5,16 +5,27 @@ using Scripts;
 
 public class UIcontrol : MonoBehaviour {
 
-	public Canvas buildingMenu;
-	public Button building1;
+	private Canvas buildingMenu;
+	private Button building1,unit1;
 	private ObjectControl objScript;
 	
 	// Use this for initialization
 	void Start () 
 	{
-		buildingMenu = buildingMenu.GetComponent<Canvas> ();
-		building1 = building1.GetComponent<Button> ();
+		buildingMenu = findUIElement("BuildingMenu",gameObject).GetComponent<Canvas> ();
+		
+		building1 = findUIElement("Building 1",buildingMenu.gameObject).GetComponent<Button> ();
+		building1.onClick.AddListener(delegate { building1_click(); });
+		
+		unit1 = findUIElement("Unit 1",buildingMenu.gameObject).GetComponent<Button> ();
+		unit1.onClick.AddListener(delegate { unit1_click(); });
+		
 		objScript = GameObject.Find("MainCamera").GetComponent<ObjectControl>();
+	}
+	
+	void Update ()
+	{
+	
 	}
 	
 	public void building1_click ()
@@ -24,5 +35,19 @@ public class UIcontrol : MonoBehaviour {
 	public void unit1_click ()
 	{
 		objScript.placeUnit("IOu_testUnit");		
+	}
+	
+	private GameObject findUIElement (string objName,GameObject parent)
+	{
+		GameObject element=null;
+		for (int i=0;i<parent.transform.childCount;i++)
+		{
+			if(parent.transform.GetChild(i).name.Equals(objName))
+			{
+				element=parent.transform.GetChild(i).gameObject;
+				break;
+			}
+		}
+		return element;
 	}
 }
